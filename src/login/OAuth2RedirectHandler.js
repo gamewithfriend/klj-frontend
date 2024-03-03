6f475fdd69e35a0a5aae5e26f531e67a5aeacbe4
gamewithfriend <jse368554545@gmail.com>
2024-03-03 22:57:26 +0900

    jth 240303 test

--- src/login/OAuth2RedirectHandler.js
+++ src/login/OAuth2RedirectHandler.js
@@ -6,11 +6,11 @@ import {NaverLogin} from "./NaverLogin.js";
 const OAuth2RedirectHandler = () => {
 
     let code = new URL(window.location.href).searchParams.get("code");
-
+    let state = new URL(window.location.href).searchParams.get("state");
     const naverLoginFunciton =  NaverLogin();
 
     useEffect(() => {
-        naverLoginFunciton(code);
+        naverLoginFunciton(code,state);
     },[]);
 
 };
