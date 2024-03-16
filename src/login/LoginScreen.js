6e13b8d181cab76cda53067bc58333a9200b274a
gamewithfriend <jse3685545@naver.com>
2024-03-17 06:39:08 +0900

    jth 240317 production setting

--- src/login/LoginScreen.js
+++ src/login/LoginScreen.js
@@ -7,13 +7,14 @@ import naver from '../assets/logo/naver.png';
 
 const LoginScreen = () => {
     const NAVER_CLIENT_ID = process.env.REACT_APP_NAVER_CLIENT_ID;
-    const REDIRECT_URI = "http://localhost:3000/login/callback/naver";
+    const REDIRECT_URI = process.env.REACT_APP_REDIRECT_URI;
     const STATE = "false";
     const NAVER_AUTH_URL = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${NAVER_CLIENT_ID}&state=${STATE}&redirect_uri=${REDIRECT_URI}`;
 
     const naverLoginClick = () => {
         window.open(NAVER_AUTH_URL,'_blank','width=700, height=600, top=50, left=50, scrollbars=yes');     
         console.log(NAVER_AUTH_URL)
+        console.log(REDIRECT_URI)
     };
 
         
