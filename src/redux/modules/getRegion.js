658e2e9656cb68aed9a5889a4e37cc6df7d69ec5
jakelee0424 <qkql006@gmail.com>
2024-04-15 21:13:18 +0900

    areaSearch edit LJK

--- src/redux/modules/getRegion.js
+++ src/redux/modules/getRegion.js
@@ -7,6 +7,8 @@ const regionSetting = (state = initialState, action) => {
   switch (action.type) {
     case "basicRegionSetting":
       return {regionList : action.payload};
+    case "resetAreaSetting":
+      return {regionList : [] };
     default:
       return state;
   }
