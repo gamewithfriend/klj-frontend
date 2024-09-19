d449be1323d1a2ae55d05876c28fdafeaaab9c14
jakelee0424 <qkql006@gmail.com>
2024-09-19 23:27:30 +0900

    add motion LJK

--- src/PTMatching/TrainerProfile.js
+++ src/PTMatching/TrainerProfile.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import { useSelector, useDispatch } from "react-redux";
+import { NavLink, useNavigate } from "react-router-dom";
 
 const TrainerProfile = () => {
 
     const reduxTrainerId = useSelector((state) => state.trainerId);
-    
+    const navigate = useNavigate(); 
+    const clickBack = () => {
+        navigate(-1); // 피드 
+    };
+
     return (
         <div>
             <p>트레이너 페이지</p>
             <p>{reduxTrainerId.trainerId}</p>
+            <button onClick={clickBack}>뒤로 가기</button>
         </div>
     );
 };
